327e92fd75abf9d7198021d42a8634f192cb640d
Abraham Dick <abedick@ku.edu>
2017-10-23 20:35:42 -0500

    Completed tutorial on rails website

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,49 @@
 class PostsController < ApplicationController
+
+    # http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
     def index
         @posts = Post.all
     end
-
-    def new
-
-    end
-
+    
     def show
         @post = Post.find(params[:id])
     end
 
+    def new
+        @post = Post.new
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+        
     def create
         @post = Post.new(post_params)
 
-        @post.save
-        redirect_to @post
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to posts_path
     end
 
     private
