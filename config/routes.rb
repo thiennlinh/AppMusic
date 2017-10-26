b875d27f5bafb90321d65faffd5ba8824b70ceca
Abraham Dick <abedick@ku.edu>
2017-10-25 20:38:20 -0500

    Simple user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   get 'welcome/index'
@@ -10,6 +12,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
