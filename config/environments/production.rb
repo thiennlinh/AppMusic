a849b851f808e3b2ed3c363b5f6c2ab3d3b996cb
Abraham Dick <abedick@ku.edu>
2017-11-02 21:40:16 -0500

    config changes for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
+  Rails.application.routes.default_url_options[:host] = 'quablex.herokuapp.com'
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
