48a65207a58bf6645b8523ca29c6ff0501ce8797
Abraham Dick <abedick@ku.edu>
2017-11-09 15:12:38 -0600

    massive upgrade and communities

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,15 @@ class StaticPagesController < ApplicationController
       @posts = current_user.microposts
     end
 
-    @feed = Micropost.paginate(page: params[:page], :per_page => 10)
+    @feed = Micropost.paginate(page: params[:page], :per_page => 5)
+    
+    #  Make sure to only collect 3 records using whatever method!
+    @community_listings = []
+    @community_names = []
+    for id in 2..4
+      @community_names.append(Community.where(id: id).select(:title, :id))
+      @community_listings.push(Micropost.where(community_id: id).limit(5))
+    end
 
   end
 
