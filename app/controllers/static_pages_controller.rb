7dee5885bd6691b7dc9890021cee4acf4b3be877
Alex Shadley <shadleyalex@gmail.com>
2017-11-20 13:46:42 -0600

    fixes add to playlist button to work with users owning playlists

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,11 @@ require 'rspotify'
 
 class StaticPagesController < ApplicationController
   def home
-    
+
     if logged_in?
       @microposts = current_user.microposts.paginate(page: params[:page])
       @posts = current_user.microposts
+	  @playlists = current_user.playlists.all
     end
 
     @feed = Micropost.paginate(page: params[:page], :per_page => 7)
@@ -29,35 +30,35 @@ class StaticPagesController < ApplicationController
 
   # def front_spotify
   @songs = Micropost.all.limit(50)
-  
+
       ## Do not store this on github!!!
       RSpotify.authenticate("7eaf81c8e2384e0f9b021058e3141882", "ed91711edce944549f3041499433f016")
-    
-  
+
+
       spotify_user = RSpotify::User.find('1224459409')
-  
-      
-  
-  
+
+
+
+
       @playlist = RSpotify::Playlist.find('1224459409','5EAbXQCZMXMLIsqKdKR25f')
       @total = @playlist.total
-  
-  
+
+
       @tracks_to_add = []
-  
-  
+
+
       for song in @songs
         @tracks = RSpotify::Track.search(song.title)
-  
+
         for track in @tracks
           if track.artists.first.name == song.artist
             @tracks_to_add.push(track)
             break
           end
         end
-        
+
       end
-  
+
       @playlist.replace_tracks!(@tracks_to_add)
 
       redirect_to spotify_path
