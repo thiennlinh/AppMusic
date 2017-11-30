be87fdfbe47cb578b1cd39de76554b09462a30b0
Abraham Dick <abedick@ku.edu>
2017-11-29 20:01:28 -0600

    tests

--- test/models/playlist_test.rb
+++ test/models/playlist_test.rb
@@ -4,4 +4,23 @@ class PlaylistTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @playlist = Playlist.new(id: 1, name: "Lorem ipsum", user_id: 1)
+  end
+
+  test "should be valid" do
+    assert !@playlist.valid?
+  end
+
+  test "name should be present" do
+    @playlist.name = "   "
+    assert_not @playlist.valid?
+  end
+
+  test "user id should be present" do
+    @playlist.user_id = nil
+    assert_not @playlist.valid?
+  end
+
 end
--- test/models/polycom_test.rb
+++ test/models/polycom_test.rb
@@ -4,4 +4,33 @@ class PolycomTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @polycom = Polycom.new(id: 1, commentable_type: "Micropost", commentable_id: 1, user_id: 1, body: "lorem")
+  end
+
+  test "should be valid" do
+    assert !@polycom.valid?
+  end
+
+  test "commentable type should be present" do
+    @polycom.commentable_type = nil
+    assert_not @polycom.valid?
+  end
+
+  test "commentable id should be present" do
+    @polycom.commentable_id = nil
+    assert_not @polycom.valid?
+  end
+
+  test "user id should be present" do
+    @polycom.user_id = nil
+    assert_not @polycom.valid?
+  end
+
+  test "body should be present" do
+    @polycom.body = nil
+    assert_not @polycom.valid?
+  end
+
 end
